f791c96ce29b99585bf673daf4684ab3d71e0983
cuongnc0211 <cuongnguyenfu@gmail.com>
2022-08-07 18:21:49 +0700

    article's form and add active_storage action_text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
 class Article < ApplicationRecord
   has_many :article_topics
   has_many :topics, through: :article_topics
+
+  has_rich_text :content
+
+  STATUSES = %w[drafting publish hiden]
+
+  enum :status, STATUSES.zip(STATUSES).to_h
 end
