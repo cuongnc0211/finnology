8726603e266ee19bf4e5c17f98bc3ca8af31a0df
cuongnc0211 <cuongnguyenfu@gmail.com>
2022-08-25 22:59:51 +0700

    fix editor style

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,12 @@ module FinnologyBlog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Load dotenv only in development or test environment
+    if ['development', 'test'].include? ENV['RAILS_ENV']
+      Dotenv::Railtie.load
+    end
+
+    HOSTNAME = ENV['HOSTNAME']
   end
 end
